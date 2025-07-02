73667c261d2073e5dd6798baacf3decfbd5eec87
Rajneesh Singh <rajneeshsingh9448@gmail.com>
2025-07-02 18:18:10 +0530

    modified components (Button , Input , Progress , TextArea , global.css )

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,45 @@
 import { Button } from "@/components/ui/button";
 import '../app/globals.css';
+import { Progress } from "@/components/ui/progress";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@radix-ui/react-checkbox";
 
 
 export default function Home() {
   return (
-  
-  
-       <Button variant="default" >
-    Hello world;
-   </Button>
 
-   
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">
+            I am a Button
+          </Button>
+        </div>
+
+        <div>
+         <Input placeholder="I am a INput">
+         </Input>
+        </div>
+
+
+        <div>
+         <Progress value ={50}>
+
+         </Progress>
+        </div>
+
+        <div>
+         <Textarea placeholder="I am a text Area"/>
+
+       
+        </div>
+
+        <div>
+         <Checkbox/>
+        </div>
+      </div>
+      </div>
 
   );
 };
